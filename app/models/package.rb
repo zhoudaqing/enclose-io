23d0ce3b7268343e960666ffc2c447d671a659bb
Minqi Pan <pmq2001@gmail.com>
2016-09-28 08:36:59 +0800

    Windows First

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,8 +2,8 @@ class Package < ApplicationRecord
   belongs_to :project
   KINDS = {
     'exe32' => 'x86.exe',
-    'linux64' => 'x64-linux',
-    'mac64' => 'x64-darwin'
+    # 'linux64' => 'x64-linux',
+    # 'mac64' => 'x64-darwin'
   }
   enum kind: KINDS.keys
   enum phase: ['doing', 'done']
