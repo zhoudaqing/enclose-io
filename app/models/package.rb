984273d9cccfffa098032d874d3bfec4975bf687
Minqi Pan <pmq2001@gmail.com>
2016-09-28 12:11:01 +0800

    linux64 is easist to tackle first

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,8 +1,8 @@
 class Package < ApplicationRecord
   belongs_to :project
   KINDS = {
-    'exe32' => 'x86.exe',
-    # 'linux64' => 'x64-linux',
+    # 'exe32' => 'x86.exe',
+    'linux64' => 'x64-linux',
     # 'mac64' => 'x64-darwin'
   }
   enum kind: KINDS.keys
