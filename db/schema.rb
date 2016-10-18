26080436b8c0509926f9875832b932974a5be51a
Minqi Pan <pmq2001@gmail.com>
2016-10-18 11:53:19 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,21 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160913082953) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "packages", force: :cascade do |t|
-    t.integer  "project_id", null: false
-    t.string   "name", null: false
+    t.integer  "project_id",             null: false
+    t.string   "name",                   null: false
     t.string   "version"
-    t.integer  "phase", default: 0, null: false
-    t.integer  "kind", null: false
+    t.integer  "phase",      default: 0, null: false
+    t.integer  "kind",                   null: false
     t.integer  "size"
     t.string   "url"
     t.datetime "done_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "projects", force: :cascade do |t|
-    t.string   "name", null: false
-    t.string   "latest_version", null: false
-    t.json     "npm_payload", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "project_users", force: :cascade do |t|
@@ -42,20 +35,29 @@ ActiveRecord::Schema.define(version: 20160913082953) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string   "name",           null: false
+    t.string   "latest_version", null: false
+    t.json     "npm_payload",    null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "login",                                  null: false
-    t.string   "name",                                   null: false
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.string   "email",               default: "", null: false
+    t.string   "login",                            null: false
+    t.string   "name",                             null: false
+    t.integer  "sign_in_count",       default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "github_uid",                             null: false
-    t.string   "github_access_token",                    null: false
-    t.json     "github_payload",                         null: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.index ["github_uid"], unique: true, using: :btree
+    t.string   "github_uid",                       null: false
+    t.string   "github_access_token",              null: false
+    t.json     "github_payload",                   null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.index ["github_uid"], name: "index_users_on_github_uid", unique: true, using: :btree
   end
+
 end
